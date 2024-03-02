406c237009dca6b03c0a569fa50e981029c66c41
blafrance42 <benlafrance42@gmail.com>
2024-03-01 20:24:32 -0800

    connected datebase to classes

---
+++ CountryModel/CountriessourceContext.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace CountryModel;
+
+public partial class CountriessourceContext : DbContext
+{
+    public CountriessourceContext()
+    {
+    }
+
+    public CountriessourceContext(DbContextOptions<CountriessourceContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<City> Cities { get; set; }
+
+    public virtual DbSet<Country> Countries { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=CountriesGolden;Integrated Security=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<City>(entity =>
+        {
+            entity.HasKey(e => e.CityId).HasName("PK__Table__F2D21B7629C254B1");
+
+            entity.HasOne(d => d.Country).WithMany(p => p.Cities)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK_City_Country");
+        });
+
+        modelBuilder.Entity<Country>(entity =>
+        {
+            entity.HasKey(e => e.CountryId).HasName("PK__Country__10D1609F03F9E987");
+
+            entity.Property(e => e.Iso2).IsFixedLength();
+            entity.Property(e => e.Iso3).IsFixedLength();
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
